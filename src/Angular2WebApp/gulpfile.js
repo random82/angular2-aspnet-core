4d12c10de937cb348bbffb5208ed7e68417590c0
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-06-05 19:45:20 +1000

    Copying Angular2 and dependencies to wwwroot/lib folder

--- src/Angular2WebApp/gulpfile.js
+++ src/Angular2WebApp/gulpfile.js
@@ -49,4 +49,50 @@ gulp.task("min:css", function () {
         .pipe(gulp.dest("."));
 });
 
+gulp.task("copy:systemjs", function () {
+    return gulp.src(paths.npmLibSrc + '/systemjs/dist/**/*.*', { base: paths.npmLibSrc + '/systemjs/dist/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/systemjs/dist/'));
+});
+ 
+gulp.task("copy:angular2", function () {
+    return gulp.src(paths.npmLibSrc + '/@angular/**/*.js', { base: paths.npmLibSrc + '/@angular/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/@angular/'));
+});
+ 
+gulp.task("copy:core-js", function () {
+    return gulp.src(paths.npmLibSrc + '/core-js/**/*min.js', { base: paths.npmLibSrc + '/core-js/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/core-js/'));
+});
+ 
+gulp.task("copy:rxjs", function () {
+    return gulp.src(paths.npmLibSrc + '/rxjs/**/*.js', { base: paths.npmLibSrc + '/rxjs/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/rxjs/'));
+});
+
+gulp.task("copy:zone.js", function () {
+    return gulp.src(paths.npmLibSrc + '/zone.js/dist/*.*', { base: paths.npmLibSrc + '/zone.js/dist/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/zone.js/dist/'));
+});
+
+gulp.task("copy:angular-in-memory", function () {
+    return gulp.src(paths.npmLibSrc + '/angular2-in-memory-web-api/*.js', { base: paths.npmLibSrc + '/angular2-in-memory-web-api/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/angular2-in-memory-web-api/'));
+});
+
+gulp.task("copy:reflect-metadata", function () {
+    return gulp.src(paths.npmLibSrc + '/reflect-metadata/*.*', { base: paths.npmLibSrc + '/reflect-metadata/' })
+         .pipe(gulp.dest(paths.npmLibDest + '/reflect-metadata/'));
+});
+
+gulp.task('copy-dep', 
+    ['copy:rxjs', 
+    'copy:angular2', 
+    'copy:systemjs', 
+    'copy:core-js',
+    'copy:zone.js', 
+    'copy:reflect-metadata',
+    'copy:angular-in-memory']);
+
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("publish", ["min", "copy-dep"]);
